454447eb0ac00eadc449e8380722b0a29d62dc47
shabrinatalita <shabrinatalita50@gmail.com>
2020-11-10 16:53:31 +0700

    MyRecyclerView

---
+++ app/src/main/java/com/shabrina/myrecyclerview/Hero.kt
@@ -0,0 +1,10 @@
+package com.shabrina.myrecyclerview
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Hero (
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
